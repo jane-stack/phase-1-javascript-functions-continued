b7134cb8210fead60e9c81176b697841c69187e8
Tuyen Ha <tuyen_ha@outlook.com>
2022-03-20 19:55:52 -0600

    commit tree b1cb34f3b91933b2cb91068feee2aa168ee15130

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(activity= 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+};
+
+const mondayWork = function(work='go to the office') {
+    return `This Monday, I will ${work}.`;
+};
+
+function wrapAdjective(highlight = "*") {
+    return function (inner = "a hard worker") {
+        return `You are ${highlight}${inner}${highlight}!`;
+    };
+}
+
+wrapAdjective()();
+wrapAdjective("||")("a dedicated programmer");
